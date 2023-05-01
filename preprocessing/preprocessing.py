3d5a28aa8ae80fd2ba1068c8ff276a8bed52da54
AshishJumbo <ashish1840@gmail.com>
2023-05-01 19:39:05 -0400

    first commit

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+teacher_problem_log_id_potential_corruption = {
+    'teacher_xid': [3141985, 3097639, 2912675, 2912675, 2856866, 2835460, 2835460, 861463, 861254, 178649, 178649,
+                    7629, 861463],
+    'problem_log_id': [144485699, 144652730, 144485699, 132248161, 153804667, 132943963, 142080265, 136466591,
+                       150674921, 142378339, 153804667, 144800293, 136466591]
+}
+
+df_fairness_graded_openresponse = pd.read_csv('../data/raw/fairness_graded_openresponse.csv')
+
+
+
+
